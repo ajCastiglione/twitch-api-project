3a38792882405153a0b2041f740e86760f082131
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-04-14 10:23:14 -0400

    scrapped the separate file bc shit wasnt working right

--- js/app.js
+++ js/app.js
@@ -207,6 +207,44 @@ $(function() {
     gameField.fadeToggle(600);
   });
 
+  /*=====================================Streamer Search Section==================================================*/
+
+  btn.on('click', evt => {
+    evt.preventDefault();
+    if (searchTerm.val() == "") return alert(`You must enter a user's name.`);
+    btn.html('Loading...');
+    getStreamerData();
+  });
+  searchTerm.on('keypress', evt => {
+    let keyCode = evt.which || evt.keyCode;
+    if (keyCode === 13) {
+      if (searchTerm.val() == "") return alert(`You must enter a user's name.`);
+      btn.html('Loading...');
+      getStreamerData();
+    }
+  });
+
+  //get username of streamer then - run GET https://api.twitch.tv/helix/streams?user_login
+  function getStreamerData() {
+    let term = searchTerm.val();
+    let query = `${api}streams?user_login=${term}`;
+    $.ajax({
+      url: query,
+      type: 'GET',
+      data: 'JSON',
+      beforeSend: xhr => xhr.setRequestHeader('Client-ID', id),
+      success: response => {
+console.log(response.data)
+      }
+    }); //end ajax
+  }
+
+
+  $("#showStreamerFields").on('click', function() {
+    gameField.hide();
+    streamerField.fadeToggle(600);
+  });
+
   /*TODO: Fix the layout of this app. Add a search based on a streamer's name to find what
   they're playing etc... Add a way to view multiple streams in a responsive grid layout.
   Begin to program the streamer search functionality.
